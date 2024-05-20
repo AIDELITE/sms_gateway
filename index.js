73129e73774e7fcc51c91794ad29067218b054c4
Aideed <mwangaideed@gmail.com>
2024-05-20 18:03:46 +0300

    Created and tested SMS gateway using AfricasTalling API

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const express = require('express');
-const http = require('http');
+const sendSMS = require('./lib/sendSMS');
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+const port = process.env.PORT;
+
+app.listen(2100, () => {
+    console.log(`App running on port:2100`);
+
+    sendSMS('+256702066819','This is a real test message');
+
+});
