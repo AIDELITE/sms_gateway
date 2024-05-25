4537954680da16426506205fd37aabe94aa5f631
Aideed <mwangaideed@gmail.com>
2024-05-25 13:52:20 +0300

    Finalized the SMS gateway API for the Uptime Monitor Application, tested and its working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const sendSMS = require('./lib/sendSMS');
+const sendmessage = require('./lib/sendSMS');
+const configs = require('./lib/config');
 
 const app = express();
 
@@ -8,11 +9,33 @@ app.use(express.urlencoded({
     extended: false
 }));
 
-const port = process.env.PORT;
+// const port = process.env.PORT;
+const port = configs.port;
 
-app.listen(2100, () => {
-    console.log(`App running on port:2100`);
+//receive external data
+app.post('/sendMsg', (data, res) => {
+    var phoneContact = typeof (data.body.phoneContact) == 'string' && data.body.phoneContact.trim().length == 12 ? data.body.phoneContact.trim() : false;
+    var msg = typeof (data.body.msg) == 'string' && data.body.msg.trim().length > 0 ? data.body.msg.trim() : false;
+    if (phoneContact && msg) {
+        sendmessage.sendSMS('+' + phoneContact, msg, (err, response) => {
+            if (err) {
+                res.status(400);
+                res.send(response);
+            } else {
+                res.status(200);
+                res.send(response);
+            }
+        })
+    } else {
+        res.status(400);
+        res.send({
+            "Error": "One of the fields was invalid"
+        });
+    }
+});
+app.listen(port, () => {
+    console.log(`App running on port:${port}`);
 
-    sendSMS('+256702066819','This is a real test message');
+    //sendSMS('+256702066819','This is a real test message');
 
 });
